c26213814f301b0531e1b608657ba72440a895a6
Gus Bonfant <gbonfant@me.com>
2015-05-12 10:42:43 +0100

    Show linenumbers

--- init.el
+++ init.el
@@ -57,6 +57,10 @@
 ; No toolbar
 (tool-bar-mode -1)
 
+; Enable line numbers
+(global-linum-mode 1)
+
+
 ; Get rid of the butt ugly OSX scrollbars in GUI
 (when (display-graphic-p) (set-scroll-bar-mode nil))
 
